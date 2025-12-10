ba0a4d6a9d2a7e50db24632bad919fec659f21f5
Jonny463 <l21212367@tectijuana.edu.mx>
2025-12-10 14:25:28 -0800

    Add files via upload

---
+++ src/Capa_Datos/IRobot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoV7.Capa_Datos
+{
+    public interface IRobot
+    {
+        int IdRobot { get; set; }
+        TareaRobot Tarea { get; set; }
+        bool EstatusActividad { get; set; }
+        TareaRobot GetTareaRobot();
+        bool getEstatusActividad();
+        string isActivo();
+        void AsignarTarea(int tipo);
+        void AsignarEstatus(bool estatus);
+        MementoRobot CrearMemento();
+        void RestaurarMemento(MementoRobot memento);
+    }
+}
